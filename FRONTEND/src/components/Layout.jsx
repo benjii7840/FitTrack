784474a121e2b0c35970ac18b2f8948d7eb4b497
Benjamin Baraza <barazabenjamin58@gmail.com>
2026-03-20 19:08:56 +0300

    changes

--- FRONTEND/src/components/Layout.jsx
+++ FRONTEND/src/components/Layout.jsx
@@ -1,10 +1,10 @@
 import { Outlet } from "react-router-dom";
-import Sidebar from "./Sidebar";
+import navbar from "./navbar";
 
 const Layout = () => {
   return (
     <div className="flex h-screen">
-      <Sidebar />
+      <navbar />
       <div className="flex-1 bg-gray-100 p-8 overflow-y-auto">
         <Outlet />
       </div>
